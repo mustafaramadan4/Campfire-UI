8688c8691de7fd9917a543c86a7b95ca4e8eb9f2
Mustafa Ramadan <ramadan.m@husky.neu.edu>
2020-07-24 15:33:29 -0700

    Recreating dashboard view from issuereport and issuelist iteration 1

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -10,12 +10,201 @@ import graphQLFetch from './graphQLFetch.js';
 import withToast from './withToast.jsx';
 import store from './store.js';
 
+const statuses = ['Reconnect with:'];
+// Still working on this to adapt to dashboard look
 class Dashboard extends React.Component {
+  static async fetchData(match, search, showError) {
+    const params = new URLSearchParams(search);
+    const vars = { hasSelection: false, selectedId: 0 };
+    if (params.get('status')) vars.status = params.get('status');
+
+    const effortMin = parseInt(params.get('effortMin'), 10);
+    if (!Number.isNaN(effortMin)) vars.effortMin = effortMin;
+    const effortMax = parseInt(params.get('effortMax'), 10);
+    if (!Number.isNaN(effortMax)) vars.effortMax = effortMax;
+
+    const { params: { id } } = match;
+    const idInt = parseInt(id, 10);
+    if (!Number.isNaN(idInt)) {
+      vars.hasSelection = true;
+      vars.selectedId = idInt;
+    }
+
+    let page = parseInt(params.get('page'), 10);
+    if (Number.isNaN(page)) page = 1;
+    vars.page = page;
+
+    const query = `query issueList(
+      $status: StatusType
+      $effortMin: Int
+      $effortMax: Int
+      $hasSelection: Boolean!
+      $selectedId: Int!
+      $page: Int
+    ) {
+      issueList(
+        status: $status
+        effortMin: $effortMin
+        effortMax: $effortMax
+        page: $page
+      ) {
+        issues {
+          id title status owner
+          created effort due
+        }
+        pages
+      }
+      issue(id: $selectedId) @include (if : $hasSelection) {
+        id description
+      }
+    }`;
+
+    const contactListQuery = `query contactList(
+      $activeStatus: Boolean
+      $page: Int
+      $hasSelection: Boolean!
+      $selectedId: Int!
+      ) {
+      contactList(page:$page, activeStatus: $activeStatus) {
+        contacts {
+          id name company title contactFrequency email
+          phone LinkedIn priority familiarity contextSpace
+          activeStatus}
+        pages
+      }
+      contact(id: $selectedId) @include (if : $hasSelection) {
+        id notes
+      }
+    }`;
+
+    // modified to contact list query
+    const data = await graphQLFetch(contactListQuery, vars, showError);
+    return data;
+  }
+
+  constructor() {
+    super();
+    const initialData = store.initialData || { contactList: {} };
+    const {
+      contactList: { contacts, pages }, contact: selectedContact,
+    } = initialData;
+    delete store.initialData;
+    this.state = {
+      contacts,
+      selectedContact,
+      pages,
+    };
+    // this.closeIssue = this.closeIssue.bind(this);
+    this.deleteIssue = this.deleteIssue.bind(this);
+  }
+
+
+  
+  componentDidMount() {
+    const { contacts } = this.state;
+    if (contacts == null) this.loadData();
+  }
+
+  componentDidUpdate(prevProps) {
+    const {
+      location: { search: prevSearch },
+      match: { params: { id: prevId } },
+    } = prevProps;
+    const { location: { search }, match: { params: { id } } } = this.props;
+    if (prevSearch !== search || prevId !== id) {
+      this.loadData();
+    }
+  }
+
+  async loadData() {
+    const { location: { search }, match, showError } = this.props;
+    const data = await IssueList.fetchData(match, search, showError);
+    if (data) {
+      this.setState({
+        // changed to contactList query and contacts
+        contacts: data.contactList.contacts,
+        // Load notes if selecting the Contact
+        selectedContact: data.contact,
+        // changed to contactList query and contacts
+        pages: data.contactList.pages,
+      });
+    }
+  }
+
   render() {
-    return(
-    "Placeholder for Dashboard!:)"
+    const { contacts } = this.state;
+    if (contacts == null) return null;
+
+    const { selectedContact, pages } = this.state;
+    const { location: { search } } = this.props;
+
+    const params = new URLSearchParams(search);
+    let page = parseInt(params.get('page'), 10);
+    if (Number.isNaN(page)) page = 1;
+    const startPage = Math.floor((page - 1) / SECTION_SIZE) * SECTION_SIZE + 1;
+    const endPage = startPage + SECTION_SIZE - 1;
+    const prevSection = startPage === 1 ? 0 : startPage - SECTION_SIZE;
+    const nextSection = endPage >= pages ? 0 : startPage + SECTION_SIZE;
+
+    const items = [];
+    for (let i = startPage; i <= Math.min(endPage, pages); i += 1) {
+      params.set('page', i);
+      items.push((
+        <PageLink key={i} params={params} activePage={page} page={i}>
+          <Pagination.Item>{i}</Pagination.Item>
+        </PageLink>
+      ));
+    }
+
+    const headerColumns = (
+      statuses.map(status => (
+        <th key={status}>{status}</th>
+      ))
+    );
+
+    const statRows = stats.map(counts => (
+      <tr key={counts.owner}>
+        <td>{counts.owner}</td>
+        {statuses.map(status => (
+          <td key={status}>{counts[status]}</td>
+        ))}
+      </tr>
+    ));
+
+    return (
+      <>
+        <Panel>
+          <Panel.Heading>
+            <Panel.Title toggle>Filter</Panel.Title>
+          </Panel.Heading>
+          <Panel.Body collapsible>
+            <IssueFilter urlBase="/dashboard" />
+          </Panel.Body>
+        </Panel>
+        <Table bordered condensed hover responsive>
+          <thead>
+            <tr>
+              <th />
+              {headerColumns}
+            </tr>
+          </thead>
+          <tbody>
+            {statRows}
+          </tbody>
+        </Table>
+      </>
     );
   }
 }
 
-export default Dashboard;
+const DashboardtWithToast = withToast(Dashboard);
+DashboardtWithToast.fetchData = Dashboard.fetchData;
+export default DashboardtWithToast;
+
+//   render() {
+//     return(
+//     "Placeholder for Dashboard!:)"
+//     );
+//   }
+// }  
+// export default Dashboard;
