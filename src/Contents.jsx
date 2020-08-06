7729bcee5465e1b0413ef28b3f5e7da8c24a5e42
Stefan Hristov <shristov22@gmail.com>
2020-08-05 20:56:58 -0700

    Redirect Routes for signin/signout completed

--- src/Contents.jsx
+++ src/Contents.jsx
@@ -13,45 +13,22 @@ import NotFound from './NotFound.jsx';
 
 
 export default function Contents({user}) {
-
-  const PrivateRoute = ({component: Component, ...rest}) => (
-    <Route {...rest} render={(props)=> (
-      (user.signedIn) === true 
-        ? <Component {...props}/>
-        : <Redirect to='/welcome'/>
-    )} />
-  )
-
-  return (
-    <Switch>
+  if (user.signedIn) {
+    return (
+      <Switch>
+        {/* Signed In available routes */}
         <Redirect exact from="/" to="/welcome" />
-        <Route path='/welcome' component={Welcome}/>
-        <PrivateRoute path='/dashboard' component={Dashboard}/>
-        <PrivateRoute path='/issues/:id?' component={IssueList}/>
-        <PrivateRoute path='/edit/:id' component={IssueEdit}/>
-        <PrivateRoute path='/report' component={IssueReport}/>
-        <Route path='/about' component={About}/>
-        <Route path='/*' component={About}/>
-    </Switch>
-  );
-
-
-  // console.log("Contesnts user signedin status:", user.signedIn)
-  // if (user.signedIn) {
-  //   return (
-  //     <Switch>
-  //       {/* Signed In available routes */}
-  //       <Redirect exact from="/" to="/welcome" />
-  //       {routes[0].map(attrs => <Route {...attrs} key={attrs.path} />)}
-  //     </Switch>
-  //   );
-  // } else {
-  //   return (
-  //     <Switch>
-  //       {/* Not Signed In available routes */}
-  //       <Redirect exact from="/" to="/welcome" />
-  //       {routes[1].map(attrs => <Route {...attrs} key={attrs.path} />)}
-  //     </Switch>
-  //   );
-  // };
+        {routes.map(attrs => <Route {...attrs} key={attrs.path} />)}
+      </Switch>
+    );
+  } else {
+    return (
+      <Switch>
+          {/* Signed Out available routes */}
+          <Redirect exact from="/" to="/welcome" />
+          <Route path='/welcome' component={Welcome}/>
+          <Redirect from="*" to="/welcome" />
+      </Switch>
+    );
+  }
 }
