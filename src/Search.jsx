96803a155c9fd88201e8254601a1941077126d11
Stefan Hristov <shristov22@gmail.com>
2020-08-05 20:58:46 -0700

    Fixed Search Filter w/ UserEmail context

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import SelectAsync from 'react-select/lib/Async'; // eslint-disable-line
 import { withRouter } from 'react-router-dom';
-
 import graphQLFetch from './graphQLFetch.js';
 import withToast from './withToast.jsx';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
-
     this.onChangeSelection = this.onChangeSelection.bind(this);
     this.loadOptions = this.loadOptions.bind(this);
   }
@@ -20,22 +18,16 @@ class Search extends React.Component {
 
   async loadOptions(term) {
     if (term.length < 3) return [];
-    // const query = `query issueList($search: String) {
-    //   issueList(search: $search) {
-    //     issues {id title}
-    //   }
-    // }`;
-
-    // Implemented Query to search contact list
-    // The indexed fields are name and company
-    const query = `query contactList($search: String) {
-      contactList(search: $search) {
+    const query = `query contactList($search: String $ownerEmail: String) {
+      contactList(search: $search ownerEmail: $ownerEmail) {
         contacts {id name company title}
       }
     }`;
-
+    // Get user email for search query
+    const {user} = this.props;
+    console.log("user from load options:", user);
     const { showError } = this.props;
-    const data = await graphQLFetch(query, { search: term }, showError);
+    const data = await graphQLFetch(query, { search: term, ownerEmail: user.email}, showError);
     return data.contactList.contacts.map(contact => ({
       label: `#${contact.id}: ${contact.name}`, value: contact.id,
     }));
